3ec6af065ab3c1a6f4694aeae3b9639baf8d7972
kalyan <kalyan2726@gmail.com>
2026-05-24 22:37:19 +0530

    love commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Working!"
+    return "CI/CD Pipeline Working i love u !"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=6000)
